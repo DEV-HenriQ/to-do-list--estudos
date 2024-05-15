6fba6807083b84967627a03f136099100daa5d4b
DEV-HenriQ <devjosehenrique@gmail.com>
2024-05-15 01:00:57 -0300

    Adicionando função nos botões de '+'

---
+++ JS/openCloseDay.js
@@ -0,0 +1,21 @@
+const buttonsPlus = document.querySelectorAll('.plus')
+buttonsPlus.forEach(function(cada)
+{
+   cada.addEventListener('click', function abrirJanelinha()
+{
+   let classePai = cada.parentNode.classList[0]
+   let inputX = 'input' + classePai.slice(5,6)
+   let divX = document.querySelector(`.${inputX}`)
+   let result = divX.classList.contains('hide')
+   if(result)
+   {
+      divX.classList.add('show')
+      divX.classList.remove('hide')
+   }
+   else
+   {
+      divX.classList.add('hide')
+      divX.classList.remove('show')
+   }
+})
+})
